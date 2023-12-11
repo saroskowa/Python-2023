3a292d656e3fc13a72134c20e5d54e72534fa63b
saroskowa <sarosiekizabela@gmail.com>
2023-12-11 12:17:43 +0100

    dołozyłam komentarz

--- 04_Instrukcje_warunkowe/wyrazenia_logiczne.py
+++ 04_Instrukcje_warunkowe/wyrazenia_logiczne.py
@@ -7,3 +7,12 @@ a > 2
 a < 3
 a >= 3
 a <= 3
+
+a = int(input("podaj liczbe"))
+b = a/10
+c = a//10
+i = a+b
+if(i % 7== 0 and a % 2== 0):
+    print("liczba jest dobra")
+else:
+    print("liczba jest zla")
