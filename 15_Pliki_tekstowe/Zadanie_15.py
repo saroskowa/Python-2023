62992bb47775bdaa760bef0712f3b81062d8aad9
saroskowa <sarosiekizabela@gmail.com>
2023-12-13 16:02:20 +0100

    Zadania

---
+++ 15_Pliki_tekstowe/Zadanie_15.py
@@ -0,0 +1,14 @@
+import csv
+import sys
+
+data = []
+
+with open(sys.argv[1]) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        data.append(row)
+
+print(
+    sum([float(x['Budget']) for x in data if x['Director'] == sys.argv[2]])
+)
+print(data)
